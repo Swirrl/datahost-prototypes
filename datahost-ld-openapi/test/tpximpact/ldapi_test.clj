5b2c823160349d15345a776b185893e8f54b254d
Scott Lowe <scottwithtwotees@gmail.com>
2023-05-03 21:54:23 -0400

    Add simple RDF4j native store to act as SPARQL backend

--- datahost-ld-openapi/test/tpximpact/ldapi_test.clj
+++ datahost-ld-openapi/test/tpximpact/ldapi_test.clj
@@ -2,20 +2,11 @@
   (:require
     [clojure.test :refer :all]
     [clj-http.client :as http]
-    [integrant.core :as ig]
-    [tpximpact.datahost.ldapi :as sut]))
+    [tpximpact.test-helper :as th]))
 
 (deftest service-sanity-test
-  (testing "LD API service starts and handles requests"
-    (let [config (sut/load-configs ["test-base-system.edn"
-                                    ;; env.edn contains environment specific
-                                    ;; overrides to the base-system.edn and
-                                    ;; is set on classpath depending on env.
-                                    "ldapi/env.edn"])
-          sys (sut/start-system config)]
-      (try
-        (let [response (http/get "http://localhost:3400/")]
-          (is (= (:status response) 200))
-          (is (= (:body response) "Hello World")))
-        (finally
-          (ig/halt! sys))))))
+  (th/with-system sys
+    (testing "LD API service starts and handles requests"
+      (let [response (http/get "http://localhost:3400/")]
+        (is (= (:status response) 200))
+        (is (= (:body response) "Hello World"))))))
