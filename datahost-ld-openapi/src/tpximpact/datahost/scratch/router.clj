d797dbd9e608c9c032c558f31456869c042b4f75
Scott Lowe <scottwithtwotees@gmail.com>
2023-05-09 16:14:39 -0400

    WIP: JSON string keys should remain so + Malli parameter validation

--- datahost-ld-openapi/src/tpximpact/datahost/scratch/router.clj
+++ datahost-ld-openapi/src/tpximpact/datahost/scratch/router.clj
@@ -11,15 +11,33 @@
             [muuntaja.core :as muu.c]
             [meta-merge.core :as mm]))
 
+(def custom-coercion
+  (mc/create
+    (-> mc/default-options
+        ;(assoc :strip-extra-keys false)
+        (assoc-in
+          [:formats "application/json" :decoder-opts]
+          {:decode-key-fn str})
+        (assoc-in
+          [:formats "application/json" :encoder-opts]
+          {:encode-key-fn str}))))
+
+(def muuntaja-instance
+  (muu.c/create
+    (-> muu.c/default-options
+        (assoc-in [:formats "application/json" :decoder-opts] {:decode-key-fn str})
+        (assoc-in [:formats "application/json" :encoder-opts] {:encode-key-fn str}))))
+
+
 (defmethod ig/init-key :tpximpact.datahost.scratch.router/router
   [_ {:keys [api-route-data] ::ring/keys [opts default-handlers handlers]}]
   (ring/ring-handler
     (ring/router api-route-data
                  (mm/meta-merge
-                   {:data {:coercion mc/coercion
-                           :muuntaja muu.c/instance
+                   {:data {:coercion custom-coercion
+                           :muuntaja muuntaja-instance
                            :middleware [parameters/parameters-middleware ;; query-params & form-params
-                                        ;content-type/wrap-content-type
+                                        content-type/wrap-content-type
                                         muuntaja/format-middleware
                                         muuntaja/format-negotiate-middleware ;; content-negotiation
                                         muuntaja/format-response-middleware ;; encoding response body
@@ -28,8 +46,11 @@
                                         rrc/coerce-exceptions-middleware
                                         rrc/coerce-request-middleware ;; coercing request parameters
                                         rrc/coerce-response-middleware ;; coercing response bodies
-                                        ]}}
+                                        ]
+                           ;:compile coercion/compile-request-coercers
+                           }}
                    opts))
     (ring/routes
       (ring/redirect-trailing-slash-handler)
-      (ring/create-default-handler default-handlers))))
+      (ring/create-default-handler default-handlers)
+      )))
