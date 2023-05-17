6d87f92b1d3883da8329b59114f697c4c929980f
Roland Sadowski <szabla@gmail.com>
2023-05-17 11:13:38 +0100

    WIP: catql: minor test changes

--- datahost-graphql/test/tpximpact/facet_search_test.clj
+++ datahost-graphql/test/tpximpact/facet_search_test.clj
@@ -1,14 +1,11 @@
 (ns tpximpact.facet-search-test
   (:require
+   [clojure.set :as set]
    [clojure.test :refer :all]
    [tpximpact.test-helpers :as h]))
 
-(deftest ^{:kaocha/pending "https://github.com/Swirrl/catql-prototype/issues/8"}
-  facet-search-test
-  (let [schema (h/catql-schema)]
-    (testing "Faceted queries"
-      (testing "with no facet constraints applied"
-        (let [result (h/execute schema "
+(def query-no-facets-constraints
+  "
 query testQuery {
   endpoint {
     catalog {
@@ -33,23 +30,10 @@ query testQuery {
       }
     }
   }
+}")
 
-}")]
-          (testing "all datasets are returned"
-            (is (= 137 (count (h/result-datasets result)))))
-
-          (testing "all facets are enabled"
-            (is (= 25 (h/facets-enabled result :themes)))
-            (is (= 22 (h/facets-enabled result :creators)))
-            (is (= 12 (h/facets-enabled result :publishers))))
-
-          (testing "facet ids are returned"
-            (is (= 25 (->> (h/facets result :themes) (map :id) count)))
-            (is (= 22 (->> (h/facets result :creators) (map :id) count)))
-            (is (= 12 (->> (h/facets result :publishers) (map :id) count))))))
-
-      (testing "with one facet constrained"
-        (let [result (h/execute schema "
+(def one-facet-constraint
+  "
 query testQuery {
   endpoint {
     catalog {
@@ -58,6 +42,7 @@ query testQuery {
         datasets {
           id
           theme
+          creator
         }
         facets {
           themes {
@@ -67,6 +52,7 @@ query testQuery {
           creators {
             id
             label
+            enabled
           }
           publishers {
             id
@@ -77,14 +63,53 @@ query testQuery {
     }
   }
 }
-")]
+")
+
+(deftest ^{:kaocha/pending "https://github.com/Swirrl/catql-prototype/issues/8"}
+  facet-search-test
+  (let [schema (h/catql-schema)]
+    (testing "Faceted queries"
+
+      (testing "with no facet constraints applied"
+        (let [result (h/execute schema query-no-facets-constraints)]
+          
+          (testing "all datasets are returned"
+            (is (= 137 (count (h/result-datasets result)))))
+
+          (testing "all facets are enabled"
+            ;; (tap> {:common-themes (set/intersection
+            ;;                        (set (map :id (h/facets result :themes)))
+            ;;                        (set (map :theme (h/result-datasets result))))
+            ;;        :num-themes (count (h/facets result :themes))})
+            ;; NOTE: original count was '25'. There are 25 themes, but
+            ;; not all are present in the dataset. See 'common-themes'
+            (is (= 23 (count (h/facets-enabled result :themes))))
+            (is (= 22 (count (h/facets-enabled result :creators))))
+            ;; (tap> {:common-publishers (set/intersection
+            ;;                            (set (map :id (h/facets result :publishers)))
+            ;;                            (set (map :publisher (h/result-datasets result))))
+            ;;        :num-publishers (count (h/facets result :publishers))})
+            ;; NOTE: original count was '12'. Comment as above.
+            (is (= 10 (count (h/facets-enabled result :publishers)))))
+
+          (testing "facet ids are returned"
+            (is (= 25 (->> (h/facets result :themes) (map :id) count)))
+            (is (= 22 (->> (h/facets result :creators) (map :id) count)))
+            (is (= 12 (->> (h/facets result :publishers) (map :id) count))))))
+
+      (testing "with one facet constrained"
+        (let [result (h/execute schema one-facet-constraint)]
           (testing "datasets are filtered by the given facet"
             (is (= 7 (count (h/result-datasets result))))
             (is (= ["http://gss-data.org.uk/def/gdp#trade"]
                    (->> result h/result-datasets (map :theme) distinct))))
 
           (testing "locking a facet doesn't disable other selections within that facet"
-            (is (= 25 (h/facets-enabled result :themes))))
+            ;; NOTE: we would need to know whether each of the facet
+            ;; values is referenced by datasets that were *not*
+            ;; returned in the query. So we cant' set 'facet.enabled'
+            ;; because we're missing data at the moment.
+            (is (= 25 (count (h/facets-enabled result :themes)))))
 
           (testing "unconstrained facets that would expand search results are enabled"
             ;; publishers and creators who publish to the trade theme
