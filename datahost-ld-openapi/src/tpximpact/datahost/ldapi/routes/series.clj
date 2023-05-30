585360379b292aae53bb4be68ffd7ad4b4692f22
Kira McLean <email@kiramclean.com>
2023-05-30 18:16:08 -0300

    separate out copy, put in placeholder handlers

--- datahost-ld-openapi/src/tpximpact/datahost/ldapi/routes/series.clj
+++ datahost-ld-openapi/src/tpximpact/datahost/ldapi/routes/series.clj
@@ -1,6 +1,7 @@
 (ns tpximpact.datahost.ldapi.routes.series
   (:require
-   [tpximpact.datahost.ldapi.handlers :as handlers]))
+   [tpximpact.datahost.ldapi.handlers :as handlers]
+   [tpximpact.datahost.ldapi.routes.copy :as copy]))
 
 (def JsonLdSchema
   [:maybe
@@ -12,30 +13,30 @@
                                   ["@base" string?]]]]]]])
 
 (defn get-series-route-config [db]
-  {:summary "Retrieve metadata for an existing dataset-series"
-   :description "A dataset series is a named dataset regardless of schema, methodology or compatibility changes"
+  {:summary copy/get-series-summary
+   :description copy/get-series-description
+   :handler (partial handlers/get-dataset-series db)
    :parameters {:path {:series-slug string?}}
    :responses {200 {:body JsonLdSchema}
-               404 {:body [:enum "Not found"]}}
-   :handler (partial handlers/get-dataset-series db)})
+               404 {:body [:enum "Not found"]}}})
 
 (defn put-series-route-config [db]
- {:summary "Create or update metadata on a dataset-series"
-             :parameters {:body JsonLdSchema
-                          :path {:series-slug string?}
-                          :query [:map
-                                  [:title {:title "Title"
-                                           :description "Title of dataset"
-                                           :optional true} string?]
-                                  [:description {:title "Description"
-                                                 :description "Description of dataset"
-                                                 :optional true} string?]]}
-             :responses {200 {:description "Series already existed and was successfully updated"
-                              :body map?}
-                         201 {:description "Series did not exist previously and was successfully created"
-                              :body map?}
-                         500 {:description "Internal server error"
-                              :body [:map
-                                     [:status [:enum "error"]]
-                                     [:message string?]]}}
-             :handler (partial handlers/put-dataset-series db)} )
+  {:summary copy/put-series-summary
+   :handler (partial handlers/put-dataset-series db)
+   :parameters {:body JsonLdSchema
+                :path {:series-slug string?}
+                :query [:map
+                        [:title {:title "Title"
+                                 :description "Title of dataset"
+                                 :optional true} string?]
+                        [:description {:title "Description"
+                                       :description "Description of dataset"
+                                       :optional true} string?]]}
+   :responses {200 {:description "Series already existed and was successfully updated"
+                    :body map?}
+               201 {:description "Series did not exist previously and was successfully created"
+                    :body map?}
+               500 {:description "Internal server error"
+                    :body [:map
+                           [:status [:enum "error"]]
+                           [:message string?]]}}})
