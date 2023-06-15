20aac038bb72f528ab8c6f51863733854ba36d41
Roland Sadowski <szabla@gmail.com>
2023-06-15 14:39:05 +0100

    ld-openapi: extract issuued+modified-dates to shared ns

--- datahost-ld-openapi/src/tpximpact/datahost/ldapi/models/shared.clj
+++ datahost-ld-openapi/src/tpximpact/datahost/ldapi/models/shared.clj
@@ -5,18 +5,23 @@
    [tpximpact.datahost.ldapi.util :as util]
    [tpximpact.datahost.ldapi.errors :as api-errors])
   (:import
-   [java.net URI]))
+   [java.net URI]
+   [java.time ZonedDateTime]))
 
 (def ld-root
   "For the prototype this item will come from config or be derived from
   it. It should have a trailing slash."
   (URI. "https://example.org/data/"))
 
+;;; ---- KEY CTORS
+
 (defn dataset-series-key [series-slug]
   (str (.getPath ld-root) series-slug))
 
 (defn release-key [series-slug release-slug]
   (str (dataset-series-key series-slug) "/" release-slug))
+
+;;; ---- CONTEXT OPS
 
 (defn normalise-context [ednld base]
   (let [normalised-context ["https://publishmydata.com/def/datahost/context"
@@ -44,6 +49,8 @@
                :expected-value base
                :actual-value base-in-doc})))
     (update ednld "@context" normalise-context base)))
+
+;;; ---- DOCUMENT OPS
 
 (def query-params->common-keys
   {:title "dcterms:title"
@@ -78,3 +85,39 @@
                       {:type ::api-errors/validation-failure
                        :supplied-id id-in-doc
                        :expected-id slug})))))
+
+
+;;; ---- ISSUED+MODIFIED DATES
+
+(def ^:private date-formatter
+  java.time.format.DateTimeFormatter/ISO_OFFSET_DATE_TIME)
+
+(defmulti -issued+modified-dates
+  "Adjusts the 'dcterms:issued' and 'dcterms:modified' of the document.
+  Ensures that the new document does not modify the issue date.
+
+  Behaviour differs when issuing a new seris and when modifying an
+  existing one."
+  (fn [_api-params old-doc _new-doc]
+    (case (some? old-doc)
+      false :issue
+      true  :modify)))
+
+(defmethod -issued+modified-dates :issue
+  [{^ZonedDateTime timestamp :op/timestamp} _ new-doc]
+  (let [ts-string (.format timestamp date-formatter)]
+    (assoc new-doc
+           "dcterms:issued" ts-string
+           "dcterms:modified" ts-string)))
+
+(defmethod -issued+modified-dates :modify
+  [{^ZonedDateTime timestamp :op/timestamp} old-doc new-doc]
+  (assoc new-doc
+         "dcterms:issued" (get old-doc "dcterms:issued")
+         "dcterms:modified" (.format timestamp date-formatter)))
+
+(defn issued+modified-dates
+  [{timestamp :op/timestamp :as api-params} old-doc new-doc]
+  {:pre [(instance? java.time.ZonedDateTime timestamp)]}
+  (-issued+modified-dates api-params old-doc new-doc))
+
