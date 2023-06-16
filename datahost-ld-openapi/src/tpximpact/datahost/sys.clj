abb82a9ca0ae7f5c7ab79d5dbee24fee595b5e02
Scott Lowe <scottwithtwotees@gmail.com>
2023-06-16 11:03:46 -0400

    LD API: Add config readers; includes #env reader

--- datahost-ld-openapi/src/tpximpact/datahost/sys.clj
+++ datahost-ld-openapi/src/tpximpact/datahost/sys.clj
@@ -1,13 +1,28 @@
 (ns tpximpact.datahost.sys
   (:require [clojure.java.io :as io]
             [meta-merge.core :as mm]
-            [integrant.core :as ig]))
+            [integrant.core :as ig])
+  (:import (java.net URI)))
+
+(defn get-env [s]
+  (System/getenv (str s)))
+
+(def readers
+  {'env get-env
+   ;; naive `or`; only takes 2 args
+   'or (fn [pair]
+         (apply #(or %1 %2) pair))
+   'uri (fn [v] (URI. v))
+   'int (fn [v]
+          (Integer/parseInt v))
+   'resource (fn [v] (io/resource v))
+   'regex (fn [r] (re-pattern r))})
 
 (defn load-system-config [config]
   (if config
-    (-> config
-        slurp
-        ig/read-string)
+    (->> config
+         slurp
+         (ig/read-string {:readers readers}))
     {}))
 
 (defn load-configs [configs]
