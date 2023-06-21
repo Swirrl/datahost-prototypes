491e1af24df71e2e489063d8a8ec59191b857177
Scott Lowe <scottwithtwotees@gmail.com>
2023-06-21 13:08:44 -0400

    LD API: Revisions tests use HTTP helper

--- datahost-ld-openapi/test/tpximpact/datahost/ldapi/models/revision_test.clj
+++ datahost-ld-openapi/test/tpximpact/datahost/ldapi/models/revision_test.clj
@@ -1,25 +1,13 @@
 (ns tpximpact.datahost.ldapi.models.revision-test
   (:require
-    [clj-http.client :as http]
     [clojure.data.json :as json]
     [clojure.test :refer [deftest is testing]]
     [tpximpact.test-helpers :as th]
     [tpximpact.datahost.ldapi.strings :as ld-str]))
 
-(defn- put-series [title]
-  (let [jsonld {"@context"
-                ["https://publishmydata.com/def/datahost/context"
-                 {"@base" "https://example.org/data/"}]
-                "dcterms:title" title}]
-    (http/put
-      (str "http://localhost:3400/data/" (ld-str/slugify title))
-      {:content-type :json
-       :body (json/write-str jsonld)})))
-
 (deftest round-tripping-revision-test
-  (th/with-system-and-clean-up sys
-    ;; SERIES
-    (put-series "my lovely series")
+  (th/with-system-and-clean-up {{:keys [GET POST PUT]} :tpximpact.datahost.ldapi.test/http-client
+                                :as sys}
 
     (let [series-title "my lovely series"
           series-slug (ld-str/slugify series-title)
@@ -29,12 +17,20 @@
                           {"@base" base}]
                          "dcterms:title" series-title}]
 
+      ;; SERIES
+      (PUT (str "/data/" (ld-str/slugify series-title))
+           {:content-type :json
+            :body (json/write-str {"@context"
+                                   ["https://publishmydata.com/def/datahost/context"
+                                    {"@base" "https://example.org/data/"}]
+                                   "dcterms:title" series-title})})
+
       (testing "Creating a revision for an existing release and series"
         ;; RELEASE
-        (let [release-url (str "http://localhost:3400/data/" series-slug "/release/release-1")
-              release-resp (http/put release-url
-                                     {:content-type :json
-                                      :body (json/write-str request-ednld)})]
+        (let [release-url (str "/data/" series-slug "/release/release-1")
+              release-resp (PUT release-url
+                                {:content-type :json
+                                 :body (json/write-str request-ednld)})]
           (is (= 201 (:status release-resp)))
 
           ;; REVISION
@@ -53,9 +49,9 @@
                                         "@id" 1,
                                         "dh:appliesToRelease" (str "../release-1")}
 
-                revision-resp (http/post revision-url
-                                         {:content-type :json
-                                          :body (json/write-str revision-ednld)})
+                revision-resp (POST revision-url
+                                    {:content-type :json
+                                     :body (json/write-str revision-ednld)})
                 inserted-revision-id (get (json/read-str (:body revision-resp)) "@id")
                 inserted-revision-url (str revision-url "/" inserted-revision-id)]
 
@@ -63,12 +59,12 @@
                 "successful post returns normalised release data")
 
             (testing "Fetching an existing revision works"
-              (let [response (http/get inserted-revision-url)]
+              (let [response (GET inserted-revision-url)]
                 (is (= 200 (:status response)))
                 (is (= normalised-revision-ld (json/read-str (:body response))))))
 
             (testing "Associated Release gets the Revision inverse triple"
-              (let [release-resp (http/get release-url)
+              (let [release-resp (GET release-url)
                     release (json/read-str (:body release-resp))]
                 (is (= (str "/data/my-lovely-series/release-1/revisions/" inserted-revision-id)
                        (get release "dh:hasRevision")))))))))))
