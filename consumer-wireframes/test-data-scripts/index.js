c5ba9caf36629bf7a6c6b4f0873efc9231bac278
Rob Chambers <robchamberspfc@gmail.com>
2023-06-22 12:55:06 +0100

    add revision for each release

--- consumer-wireframes/test-data-scripts/index.js
+++ consumer-wireframes/test-data-scripts/index.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const data = require('./data/series.json');
-// const openAPI = "http://localhost:3000";
-const openAPI = "https://ldapi-prototype.gss-data.org.uk"
+const openAPI = "http://localhost:3000";
+// const openAPI = "https://ldapi-prototype.gss-data.org.uk"
 
 createSeries = async () => {
     for (i = 0; i < data.length; i++) {
@@ -49,6 +49,38 @@ createRelease = async () => {
     }
 }
 
+createRevision = async () => {
+    body = `
+    {"@context": [
+      "https://publishmydata.com/def/datahost/context",
+      {
+        "@base": "https://example.org/data/population-aged-16-to-64-years-level-3-or-above-qualifications/"
+      }
+    ],
+    "dcterms:title": "Revision"}`
+
+    for (i = 0; i < data.length; i++) {
+        let series = ""
+        let releases = data[i].releases
+        if (data[i].id == null) {
+            series = `dataset-${i+1}`
+        } else {
+            series = data[i].id
+        }
+        for (j = 0; j < releases.length; j++) {
+            let id = releases[j].id
+            let url = `${openAPI}/data/${series}/release/${id}/revisions`
+            const response = await fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(body)
+            });
+            const api = await response.json();
+            let revision = api["@id"]
+            console.log(`Created: ${url}/${revision}`)
+        }
+    }
+}
+
 start = async () => {
     console.log("Starting: Loading Dataset Series")
         await createSeries()
@@ -57,6 +89,11 @@ start = async () => {
     console.log("Starting: Loading Dataset Releases")
         await createRelease()
     console.log("Complete: Loading Dataset Releases")
+
+    console.log("Starting: Creating revision for each Release")
+    await createRevision()
+    console.log("Complete: Creating revision for each Release")
+
 }
 
 start()
