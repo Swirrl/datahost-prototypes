2dd59541cb81a6445c0ba2be00c8f6952073ecf8
Rob Chambers <robchamberspfc@gmail.com>
2023-07-05 14:41:24 +0100

    refactor script

--- consumer-wireframes/test-data-scripts/index.js
+++ consumer-wireframes/test-data-scripts/index.js
@@ -3,21 +3,30 @@ const data = require('./data/series.json');
 const FormData = require('form-data');
 const fs = require('fs');
 
-const openAPI = "http://localhost:3000";
-// const openAPI = "https://ldapi-prototype.gss-data.org.uk"
+// const openAPI = "http://localhost:3000";
+const openAPI = "https://ldapi-prototype.gss-data.org.uk"
 
-createSeries = async () => {
-    for (i = 0; i < data.length; i++) {
-        let title = data[i].title
-        let id = ""
-        if (data[i].id == null) {
-            id = `dataset-${i+1}`
-        } else {
-            id = data[i].id
-        }
-        let description = data[i].description
+createSeries = async (series) => {
+    let title = data[i].title
+    let description = data[i].description
 
-        let url = `${openAPI}/data/${id}?title=${title}&description=${description}`
+    let url = `${openAPI}/data/${series}?title=${title}&description=${description}`
+    const response = await fetch(url, {
+        method: 'PUT'
+    });
+    const api = await response.json();
+
+    url = url.split("?")[0]
+    console.log(`Created: ${url}`)
+
+}
+
+createRelease = async (series) => {
+    let releases = data[i].releases
+    for (j = 0; j < releases.length; j++) {
+        let title = releases[j].title
+        let id = releases[j].id
+        let url = `${openAPI}/data/${series}/releases/${id}?title=${title}`
         const response = await fetch(url, {
             method: 'PUT'
         });
@@ -25,32 +34,8 @@ createSeries = async () => {
 
         url = url.split("?")[0]
         console.log(`Created: ${url}`)
-    }
-}
 
-createRelease = async () => {
-    for (i = 0; i < data.length; i++) {
-        let series = ""
-        let releases = data[i].releases
-        if (data[i].id == null) {
-            series = `dataset-${i+1}`
-        } else {
-            series = data[i].id
-        }
-        for (j = 0; j < releases.length; j++) {
-            let title = releases[j].title
-            let id = releases[j].id
-            let url = `${openAPI}/data/${series}/releases/${id}?title=${title}`
-            const response = await fetch(url, {
-                method: 'PUT'
-            });
-            const api = await response.json();
-
-            url = url.split("?")[0]
-            console.log(`Created: ${url}`)
-            
-            const postSchemas = await createSchemas(releases, series)
-        }
+        await createSchemas(releases, series)
     }
 }
 
@@ -78,16 +63,9 @@ createSchemas = async (releases, series) => {
     }
 }
 
-createRevision = async () => {
-    for (i = 0; i < data.length; i++) {
-        let series = ""
-        let releases = data[i].releases
-        if (data[i].id == null) {
-            series = `dataset-${i+1}`
-        } else {
-            series = data[i].id
-        }
-        body = `
+createRevision = async (series) => {
+    let releases = data[i].releases
+    body = `
         {"@context": [
           "https://publishmydata.com/def/datahost/context",
           {
@@ -96,56 +74,57 @@ createRevision = async () => {
         ],
         "dcterms:title": "Revision"}`
 
-        for (j = 0; j < releases.length; j++) {
-            let id = releases[j].id
-            let url = `${openAPI}/data/${series}/releases/${id}/revisions`
-            const response = await fetch(url, {
-                method: 'POST',
-                body: JSON.stringify(body)
-            });
-            const api = await response.json();
+    for (j = 0; j < releases.length; j++) {
+        let id = releases[j].id
+        let url = `${openAPI}/data/${series}/releases/${id}/revisions`
+        const response = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(body)
+        });
+        const api = await response.json();
 
-            let revision = api["@id"]
-            console.log(`Created: ${url}/${revision}`)
+        let revision = api["@id"]
+        console.log(`Created: ${url}/${revision}`)
 
+        await uploadData(releases, revision, url)
 
+    }
+}
 
-            //upload files to revision where provided
-            if (releases[j].file != null) {
-                const formData = new FormData();
-                file = releases[j].file
-                formData.append('appends', fs.createReadStream(file));
-                const settings = {
-                    method: 'POST',
-                    body: formData
-                };
-                try {
-                    url = `${url}/${revision}/changes`
-                    const fetchResponse = await fetch(url, settings);
-                    const data = await fetchResponse.json();
+uploadData = async (releases, revision, url) => {
+    if (releases[j].file != null) {
+        const formData = new FormData();
+        file = releases[j].file
+        formData.append('appends', fs.createReadStream(file));
+        const settings = {
+            method: 'POST',
+            body: formData
+        };
+        try {
+            url = `${url}/${revision}/changes`
+            const fetchResponse = await fetch(url, settings);
+            const data = await fetchResponse.json();
 
-                    console.log(`Added data to: ${url}/${revision}`)
-                } catch (e) {
-                    console.log(e)
-                    return e;
-                }
-            }
+            console.log(`Added data to: ${url}/${revision}`)
+        } catch (e) {
+            console.log(e)
+            return e;
         }
     }
 }
 
 start = async () => {
-    console.log("Starting: Loading Dataset Series")
-    await createSeries()
-    console.log("Complete: Loading Dataset Series")
-
-    console.log("Starting: Loading Dataset Releases")
-    await createRelease()
-    console.log("Complete: Loading Dataset Releases")
-
-    console.log("Starting: Creating revision for each Release")
-    await createRevision()
-    console.log("Complete: Creating revision for each Release")
+    for (i = 0; i < data.length; i++) {
+        let series = ""
+        if (data[i].id == null) {
+            series = `dataset-${i+1}`
+        } else {
+            series = data[i].id
+        }
+        await createSeries(series)
+        await createRelease(series)
+        await createRevision(series)
+    }
 }
 
 start()
