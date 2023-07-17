30f94405857ff51d8decd2e5f1f434c4865c38be
lkitching <lee@swirrl.com>
2023-07-17 17:13:55 +0100

    Remove unused code
    Issue #159 - Remove the unused release-schema, util.rdf and
    util.collections namespaces. Remove various unused functions
    and schema definitions used throughout the rest of the codebase.

--- datahost-ld-openapi/src/tpximpact/datahost/ldapi/schemas/release.clj
+++ datahost-ld-openapi/src/tpximpact/datahost/ldapi/schemas/release.clj
@@ -1,16 +1,4 @@
-(ns tpximpact.datahost.ldapi.schemas.release
-  (:require
-   [malli.core :as m]
-   [malli.util :as mu]
-   [tpximpact.datahost.ldapi.schemas.common :refer [registry]]))
-
-;;; ---- API SCHEMA
-
-(def ApiPathParams
-  (m/schema [:map 
-             [:series-slug :datahost/slug-string]
-             [:release-slug :datahost/slug-string]]
-            {:registry registry}))
+(ns tpximpact.datahost.ldapi.schemas.release)
 
 (def ApiQueryParams
   [:map
@@ -20,31 +8,3 @@
    [:description {:title "Description"
                   :description "Description of release"
                   :optional true} string?]])
-
-(def ApiParams
-  (mu/merge
-   ApiPathParams
-   ApiQueryParams))
-
-(def api-params-valid? (m/validator ApiParams))
-
-;;; ---- MODEL SCHEMA
-
-(def UpsertKeys
-  [:map
-   [:series :string]
-   [:release :string]])
-
-(def UpsertArgs
-  (let [db-schema [:map {}]
-        api-params-schema (m/schema [:map
-                                     [:op/timestamp :datahost/timestamp]
-                                     [:op.upsert/keys UpsertKeys]]
-                                    {:registry registry})
-        input-jsonld-doc-schema [:maybe [:map {}]]]
-    [:catn
-     [:db db-schema]
-     [:api-params api-params-schema]
-     [:jsonld-doc input-jsonld-doc-schema]]))
-
-(def upsert-args-valid? (m/validator UpsertArgs))
--- datahost-ld-openapi/src/tpximpact/datahost/ldapi/schemas/series.clj
+++ datahost-ld-openapi/src/tpximpact/datahost/ldapi/schemas/series.clj
@@ -1,16 +1,4 @@
-(ns tpximpact.datahost.ldapi.schemas.series
-  (:require
-   [malli.core :as m]
-   [malli.util :as mu]
-   [tpximpact.datahost.ldapi.schemas.common :refer [registry]]))
-
-;;; ---- API SCHEMA
-
-(def ApiPathParams
-  (m/schema
-   [:map
-    [:series-slug :datahost/slug-string]]
-   {:registry registry}))
+(ns tpximpact.datahost.ldapi.schemas.series)
 
 (def ApiQueryParams
   "Query parameters to the series endpoint"
@@ -22,30 +10,4 @@
                   :description "Description of dataset series"
                   :optional true} string?]])
 
-(def ApiParams
-  "API params of the series endpoint."
-  (mu/merge
-   ApiPathParams
-   ApiQueryParams))
 
-;;; ---- MODEL SCHEMA
-
-(def UpsertKeys
-  [:map
-   [:series :string]])
-
-(def UpsertArgs
-  (let [db-schema [:map {}]
-        api-params-schema (mu/merge
-                           ApiParams
-                           (m/schema [:map  
-                                      [:op/timestamp :datahost/timestamp]
-                                      [:op.upsert/keys UpsertKeys]]
-                                     {:registry registry}))
-        input-jsonld-doc-schema [:maybe [:map {}]]]
-    [:catn
-     [:db db-schema]
-     [:api-params api-params-schema]
-     [:jsonld-doc input-jsonld-doc-schema]]))
-
-(def upsert-args-valid? (m/validator UpsertArgs))
