1960f331824a4c9133263e28fca4761ea6583011
Rob Chambers <robchamberspfc@gmail.com>
2023-07-18 09:05:41 +0100

    add ability for corrections and lots of buf fixing and tidying

--- consumer-wireframes/compare-csv/index.js
+++ consumer-wireframes/compare-csv/index.js
@@ -3,17 +3,23 @@ const fs = require('fs')
 const crypto = require('crypto'),
     hash = crypto.getHashes();
 const results = [];
-const outputFile = 'test.csv'
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 const args = process.argv.slice(2);
 const original = args[0];
 const current = args[1];
+let outputPrefix = args[2];
+const observations = args[3];
+const test = args[4];
 
-console.log(original)
+if (test === "test") {
+    outputPrefix = "test-data/outputs/" + outputPrefix
+} else {
+    outputPrefix = "outputs/" + outputPrefix
+}
 
 console.time("Total time");
-const observations = "Aged 16 to 64 years level 3 or above qualifications"
+// const observations = "Aged 16 to 64 years level 3 or above qualifications"
 
 getOldData = async (url) => {
     return new Promise(function (resolve, reject) {
@@ -40,13 +46,15 @@ getCurrentData = async (url) => {
 }
 
 fullRowHash = async (data, type) => {
+
     let temp = []
     for (i = 0; i < data.length; i++) {
         let toHash = JSON.stringify(data[i])
+        toHash = toHash.toLowerCase()
         let rowHash = crypto.createHash('sha1').update(toHash).digest('hex');
         //we only need the hash for the old data so just produce an array of those
         if (type === "old") {
-            temp[i] = data[i]["fullHash"] = rowHash;
+            temp[i] = rowHash;
         } else {
             temp = data
             temp[i]["fullHash"] = rowHash;
@@ -58,25 +66,68 @@ fullRowHash = async (data, type) => {
 compareData = async (old, current) => {
     data = []
     for (i = 0; i < current.length; i++) {
+        if (i % 10000 === 0) {
+            console.log(`Compared: ${i}`)
+        }
         let hash = current[i].fullHash
-        let result = old.find(fullHash => fullHash === hash);
-        if (result == undefined) {
+        result = old.indexOf(hash)
+        if (result === -1) {
             data.push(current[i])
         }
     }
     return data
 }
 
+compareDataCorrections = async (old, current) => {
+    corrections = []
+    appends = []
+    for (i = 0; i < current.length; i++) {
+        let hash = current[i].fullHash
+        result = old.indexOf(hash)
+        if (result != -1) {
+            //dimensions match so must be a correction
+            corrections.push(i)
+        } else {
+            //dimensions don't match so assume it is new
+            appends.push(i)
+        }
+    }
+    return [corrections, appends]
+}
+
 generateHeaders = async (row) => {
-    let headers = []
-    //using row.length-1 to not include hash in output
-    for (i = 0; i < row.length - 1; i++) {
+    let headersOutput = []
+    for (i = 0; i < row.length; i++) {
         let item = {}
         item["id"] = row[i];
         item["title"] = row[i];
-        headers.push(item)
+        headersOutput.push(item)
     }
-    return headers
+    return headersOutput
+}
+
+stripOutHash = async (data) => {
+    for (i = 0; i < data.length; i++) {
+        delete data[i]["fullHash"];
+    }
+    return data
+}
+
+
+removeObservationsForHashing = (data, observationColumn) => {
+    h = Object.keys(data[0])
+    let removedObservations = []
+    for (i = 0; i < data.length; i++) {
+        let item = {}
+        for (j = 0; j < h.length; j++) {
+            let header = h[j]
+            if (header != observationColumn) {
+                item[header] = data[i][header];
+            }
+        }
+        removedObservations.push(item)
+    }
+    return removedObservations
 }
 
 start = async () => {
@@ -93,27 +144,94 @@ start = async () => {
     let currentDataHash = await fullRowHash(currentData)
     console.timeEnd("Generating hashed values");
 
+
     console.log("Compare data")
     console.time("Compare data");
     let noExact = await compareData(existingDataHash, currentDataHash)
     console.timeEnd("Compare data");
 
-    console.log("Generate download")
-    console.time("Generate download");
-    let headers = await generateHeaders(Object.keys(existingData[0]))
-    const csvWriter = createCsvWriter({
-        path: outputFile,
-        header: headers
-    });
+    let noExactNoHash = await stripOutHash(noExact)
 
-    csvWriter.writeRecords(noExact)
-        .then(() => {
-            console.log('Complete');
-        }).catch(function (error) {
-            console.log(error);
-        });
-    console.timeEnd("Generate download");
-    console.timeEnd("Total time");
+    //if there are no changes don't do anything else
+    if (noExactNoHash != 0) {
+
+        const existingDataDimensions = await removeObservationsForHashing(existingData, observations)
+        const currentDataDimensions = await removeObservationsForHashing(noExactNoHash, observations)
+
+        console.log("Generating hashed values")
+        console.time("Generating hashed values");
+        let existingDataHashDimensions = await fullRowHash(existingDataDimensions, "old")
+        let currentDataHashDimensions = await fullRowHash(currentDataDimensions)
+        console.timeEnd("Generating hashed values");
+
+        console.log("Compare data")
+        console.time("Compare data");
+        let [correctionsIndex, appendsIndex] = await compareDataCorrections(existingDataHashDimensions, currentDataHashDimensions)
+        console.timeEnd("Compare data");
+
+        console.log("Generate downloads")
+        console.time("Generate downloads");
+        let corrections = []
+        for (i = 0; i < correctionsIndex.length; i++) {
+            temp = correctionsIndex[i]
+            corrections.push(noExactNoHash[temp])
+        }
+
+        let appends = []
+        for (i = 0; i < appendsIndex.length; i++) {
+            temp = appendsIndex[i]
+            appends.push(noExactNoHash[temp])
+        }
+
+
+        if (appends.length != 0) {
+            let appendsHeader = await generateHeaders(Object.keys(appends[0]))
+            const csvWriter = createCsvWriter({
+                path: outputPrefix + "-appends.csv",
+                header: appendsHeader
+            });
+
+            if (test === "test") {
+                console.log("Appends:")
+                console.log(appends)
+                console.log("--------")
+            }
+
+
+            csvWriter.writeRecords(appends)
+                .then(() => {
+                    console.log('Complete appends file created');
+                }).catch(function (error) {
+                    console.log(error);
+                });
+        }
+
+        if (corrections.length != 0) {
+            let correctionsHeader = await generateHeaders(Object.keys(corrections[0]))
+            const csvWriter2 = createCsvWriter({
+                path: outputPrefix + "-corrections.csv",
+                header: correctionsHeader
+            });
+
+            if (test === "test") {
+                console.log("Corrections:")
+                console.log(corrections)
+                console.log("--------")
+            }
+
+            csvWriter2.writeRecords(corrections)
+                .then(() => {
+                    console.log('Complete corrections file created');
+                }).catch(function (error) {
+                    console.log(error);
+                });
+
+            console.timeEnd("Generate downloads");
+            console.timeEnd("Total time");
+        }
+    } else {
+        console.log("No changes between datasets")
+    }
 }
 
 start()
--- consumer-wireframes/compare-csv/readme.md
+++ consumer-wireframes/compare-csv/readme.md
@@ -1,14 +1,19 @@
 
 Prototype for comparing 2 csvs in js.
 
-Currently will identify, given 2 files with the same headings (in the same order) which rows in the 2nd csv do not exactly match a row in the first csv.
+Currently will identify, given 2 files with the same headings (in the same order) which rows in the 2nd csv are observation corrections or appends (new combinations of dimensions) in the first CSV and output a csv for each.
 
-`node index.js {original file path} {current file path}`
+`node index.js {original file path} {current file path} {output-name-prefix} {observation column name} {test}`
 
-e.g.
+`{test}` is optional and will just store in the test location
 
-`node index.js data/test-original.csv data/test-new.csv`
-`node index.js data/original.csv data/new.csv`
-`node index.js data/data-2019.csv data/data-2020.csv`
+Test data
 
+`node index.js test-data/append-correction/test-original.csv test-data/append-correction/test-new.csv append-correction 'Aged 16 to 64 years level 3 or above qualifications' test`
+
+`node index.js test-data/just-append/test-original.csv test-data/just-append/test-new.csv just-append 'Aged 16 to 64 years level 3 or above qualifications' test`
+
+`node index.js test-data/just-correction/test-original.csv test-data/just-correction/test-new.csv just-correction 'Aged 16 to 64 years level 3 or above qualifications' test`
+
+`node index.js test-data/no-change/test-original.csv test-data/no-change/test-new.csv no-change 'Aged 16 to 64 years level 3 or above qualifications' test`
 
