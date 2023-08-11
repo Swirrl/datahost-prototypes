7b7bc6f92199722c1469c2ae3e7a7e8a3dc42707
Roland Sadowski <szabla@gmail.com>
2023-08-11 11:57:45 +0100

    ldapi: use common-io directory deletion method

--- datahost-ld-openapi/src/tpximpact/datahost/ldapi/files.clj
+++ datahost-ld-openapi/src/tpximpact/datahost/ldapi/files.clj
@@ -1,19 +1,11 @@
 (ns tpximpact.datahost.ldapi.files
   (:import [java.io File]
            [java.nio.file Files]
-           [java.nio.file.attribute FileAttribute]))
+           [java.nio.file.attribute FileAttribute]
+           [org.apache.commons.io FileUtils]))
 
 (defn delete-dir [^File dir]
-  (doseq [f (.listFiles dir)]
-    (cond (.isFile f)
-          (.delete f)
-
-          (.isDirectory f)
-          (delete-dir f)
-
-          :else
-          nil))
-  (.delete dir))
+  (FileUtils/deleteDirectory dir))
 
 (defn create-temp-directory [prefix]
   (let [dir-path (Files/createTempDirectory prefix (make-array FileAttribute 0))]
