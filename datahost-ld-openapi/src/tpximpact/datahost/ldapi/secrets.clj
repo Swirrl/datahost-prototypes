a5a9972b7a799ef82eef670696acefc53e04131e
Andrew Mcveigh <me@andrewmcveigh.com>
2023-08-16 10:26:45 +0100

    Only use basic auth hash in auth profile

--- datahost-ld-openapi/src/tpximpact/datahost/ldapi/secrets.clj
+++ datahost-ld-openapi/src/tpximpact/datahost/ldapi/secrets.clj
@@ -1,7 +1,15 @@
 (ns tpximpact.datahost.ldapi.secrets
+  (:require [clojure.spec.alpha :as s]
+            [integrant.core :as ig])
   (:import [com.google.cloud.secretmanager.v1
-            ProjectName SecretName SecretVersionName SecretManagerServiceClient])
-  (:require [integrant.core :as ig]))
+            ProjectName SecretName SecretVersionName SecretManagerServiceClient]))
+
+(s/def ::gcloud-project string?)
+(s/def ::secret-name string?)
+(s/def ::version string?)
+
+(defmethod ig/pre-init-spec ::basic-auth-hash [_]
+  (s/keys :req-un [::gcloud-project ::secret-name ::version]))
 
 (defmethod ig/init-key ::basic-auth-hash
   [_ {:keys [gcloud-project secret-name version]}]
