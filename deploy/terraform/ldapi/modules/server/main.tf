f41e4e0ac6c038801090b60c149680d9f2507420
Andrew Mcveigh <me@andrewmcveigh.com>
2023-08-22 10:24:03 +0100

    Correct ldapi service account permissions

--- deploy/terraform/ldapi/modules/server/main.tf
+++ deploy/terraform/ldapi/modules/server/main.tf
@@ -50,14 +50,16 @@ resource "google_service_account" "ldapi_service_account" {
   display_name = "Service account to run the ldapi server"
 }
 
-resource "google_project_iam_member" "ldapi_service_account_permissions" {
+# service account needs to be able to read images from the swirrl-devops-infrastructure project
+resource "google_project_iam_member" "ldapi_service_account_permissions_acr" {
   project = "swirrl-devops-infrastructure-1"
-  for_each = toset([
-    # service account needs to be able to read images from the swirrl-devops-infrastructure project
-    "roles/artifactregistry.reader",
-    "roles/secretmanager.secretAccessor",
-  ])
-  role = each.key
+  role = "roles/artifactregistry.reader"
+  member = "serviceAccount:${google_service_account.ldapi_service_account.email}"
+}
+
+resource "google_project_iam_member" "ldapi_service_account_permissions_secrets" {
+  project = "swirrl-ons-datahost"
+  role = "roles/secretmanager.secretAccessor"
   member = "serviceAccount:${google_service_account.ldapi_service_account.email}"
 }
 
