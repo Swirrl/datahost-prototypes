972dcbcf479aff7692c8d2870a9549cb58a0f94a
Rick Moynihan <rick.m@swirrl.com>
2023-09-28 17:50:19 +0100

    Add descriptions for release routes

--- datahost-ld-openapi/src/tpximpact/datahost/ldapi/routes/release.clj
+++ datahost-ld-openapi/src/tpximpact/datahost/ldapi/routes/release.clj
@@ -10,7 +10,8 @@
 (def internal-server-error-desc "Internal server error")
 
 (defn get-release-list-route-config [triplestore system-uris]
-  {:summary "All releases metadata in the given series"
+  {:summary "List all releases metadata in the given series"
+   :description "List all releases metadata in the given dataset-series."
    :handler (partial handlers/get-release-list triplestore system-uris)
    :parameters {:path [:map routes-shared/series-slug-param-spec]}
    :responses {200 {:content {"application/ld+json" string?}}
@@ -19,6 +20,16 @@
 
 (defn get-release-route-config [triplestore change-store system-uris]
   {:summary "Retrieve metadata for an existing release"
+   :description "Given a dataset-series slug and a release slug,
+return data or metadata associated with a release.
+
+The data associated with the latest release and revision can obtained
+by issuing a request with an `Accept` header of `text/csv`, this will
+result in a redirection to the latest revision of the data.
+
+The metadata associated with the release can be obtained by issuing a
+request with an `Accept` header of `application/ld+json`, which will
+return a JSON/LD metadata document describing the release resource."
    :handler (partial handlers/get-release triplestore change-store system-uris)
    :middleware [[(partial middleware/csvm-request-response triplestore system-uris) :csvm-response]
                 [(partial middleware/entity-uris-from-path system-uris #{:dh/Release}) :entity-uris]]
@@ -34,6 +45,18 @@
 
 (defn put-release-route-config [clock triplestore system-uris]
   {:summary "Create or update metadata for a release"
+   :description "Create or update the specified release in the given
+dataset-series with the supplied metadata.
+
+If the release did not previously exist and the data is valid a new
+release will be created in the specified dataset-series.
+
+If the release previously did exist the metadata document associated
+with it will be updated.
+
+NOTE: some fields of metadata are considered \"managed\" and will be
+managed by the Datahost platform; for example Datahost will add and
+set `dcterms:modified` times for you."
    :handler (partial handlers/put-release clock triplestore system-uris)
    :middleware [[middleware/json-only :json-only]
                 [(partial middleware/flag-resource-exists triplestore system-uris
@@ -62,6 +85,9 @@
 (defn get-release-ld-schema-config
   [triplestore system-uris]
   {:summary "Retrieve release schema"
+   :description "Returns the Datahost TableSchema associated with the release.
+
+NOTE: Datahost tableschemas are extended subsets of CSVW:TableSchema."
    :handler (partial handlers/get-release-schema triplestore system-uris)
    :parameters {:path [:map
                        routes-shared/series-slug-param-spec
@@ -74,6 +100,9 @@
 (defn post-release-ld-schema-config
   [clock triplestore system-uris]
   {:summary "Create schema for a release"
+   :description "Associates a Datahost TableSchema with the specified release.
+
+The supplied document should conform to the Datahost TableSchema."
    :handler (partial handlers/post-release-schema clock triplestore system-uris)
    ;; NOTE: file schema JSON content string is validated within the handler itself
    :parameters {:body routes-shared/LdSchemaInput
