fe893bf52a2c8b8fc192c74889780a6181e12fd6
Rob Chambers <robchamberspfc@gmail.com>
2023-10-04 14:18:56 +0100

    update this branch to use dev API

--- consumer-wireframes/consumer-wireframes/assets/js/config.js
+++ consumer-wireframes/consumer-wireframes/assets/js/config.js
@@ -1,2 +1,3 @@
 // const domain = "http://localhost:3000"
-const domain = "https://ldapi-prototype.gss-data.org.uk"
+// const domain = "https://ldapi-prototype.gss-data.org.uk"
+const domain = "https://ldapi-dev.gss-data.org.uk"
