4d46f8b40cc77e22b771af5227f0b225321774af
Roland Sadowski <szabla@gmail.com>
2023-11-06 10:12:13 +0000

    ldapi: more renames in hurl scripts

--- datahost-ld-openapi/test-hurl/tpximpact/datahost/ldapi/integration_test.clj
+++ datahost-ld-openapi/test-hurl/tpximpact/datahost/ldapi/integration_test.clj
@@ -6,14 +6,15 @@
    [tpximpact.datahost.ldapi.test-util.hurl :as hurl :refer [hurl]]))
 
 (deftest minimal-tests
-  (th/with-system-and-clean-up {http-port :tpximpact.datahost.ldapi.jetty/http-port :as sys}
-    (let [variables {:host_name (str "localhost:" http-port)
-                     :series (str "my-series-" (random-uuid))
-                     :auth_token "ignore"}]
+  (testing "verify we plain `hurl` call works"
+    (th/with-system-and-clean-up {http-port :tpximpact.datahost.ldapi.jetty/http-port :as sys}
+      (let [variables {:host_name (str "localhost:" http-port)
+                       :series (str "my-series-" (random-uuid))
+                       :auth_token "ignore"}]
 
-      (let [result (hurl {:variables variables :script "bin/hurl-data/minimal.hurl"
-                          :report-junit "test-results/hurl.xml"})]
-        (is (= 0 (:exit result)))))))
+        (let [result (hurl {:variables variables :script "hurl-scripts/int-001.hurl"
+                            :report-junit "test-results/hurl.xml"})]
+          (is (= 0 (:exit result))))))))
 
 (deftest integration-tests
   (th/with-system-and-clean-up {http-port :tpximpact.datahost.ldapi.jetty/http-port :as sys}
