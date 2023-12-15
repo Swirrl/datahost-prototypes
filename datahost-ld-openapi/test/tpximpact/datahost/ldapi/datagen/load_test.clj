fdf0cfa9c000bed1778ea472665e68e7af290fd8
Tayyeb Rafique <tayyebrafique@Tayyebs-MacBook-Pro.local>
2023-12-15 14:39:42 +0000

    deleted reports

--- datahost-ld-openapi/test/tpximpact/datahost/ldapi/datagen/load_test.clj
+++ datahost-ld-openapi/test/tpximpact/datahost/ldapi/datagen/load_test.clj
@@ -4,6 +4,33 @@
 
 
 (def path "test/tpximpact/datahost/ldapi/datagen/issue-310/")
+(def seed (System/currentTimeMillis))
+;; Documentation
+
+;; GENERATE DATA
+;; - (generate x y repeats filename seed)
+;; x: int number of desired columns
+;; y: int number of desired rows
+;; repeats: int (only relevant for AD)
+;; filename: string with no spaces
+;; seed: int used for regenerating the same dataset again
+
+;; This will generate a schema and corresponding csv in datagen/issue-310
+;; It will also generate two hurl scripts:
+;; A: Append, tests a single append
+;; AD: Append Delete, appends and deletes the csv for as many repeats as specified
+
+;; GENERATE SPLIT DATA
+;; - (generate-split x y splits filename seed repeats)
+;; splits: int how many csv's the data should be divided into
+
+;; This will generate a schema and a number corresponding csv's in datagen/issue-310
+;; It will also generate a hurl script:
+;; CA: Consecutive Appends, posts all the split csvs one after the other
+
+;; NOTES
+;; - The command for running the hurl scripts can be found at the top of each one
+;; - It takes a few minutes to create data for > 5 mil, let it run or the csv will be incomplete.
 
 (defn generate [x y repeats filename seed]
   (datagen/get-data x y path filename seed)
