d03652a58fe765c463186536a436569bfb24fe2b
Roland Sadowski <szabla@gmail.com>
2023-12-27 13:12:05 +0100

    ldapi: rename column in test

--- datahost-ld-openapi/test/tpximpact/datahost/ldapi/handlers/delta_test.clj
+++ datahost-ld-openapi/test/tpximpact/datahost/ldapi/handlers/delta_test.clj
@@ -11,25 +11,25 @@
 (deftest diffing-datasets
   (let [row-schema (data.validation/make-row-schema-from-json
                     {"dh:columns" [{"@type" "dh:DimensionColumn"
-                                    "csvw:name" "attr1"
+                                    "csvw:name" "dim1"
                                     "csvw:required" true
                                     "csvw:datatype" :string
-                                    "csvw:titles" "attr1"}
+                                    "csvw:titles" "dim1"}
                                    {"@type" "dh:MeasureColumn"
                                     "csvw:name" "measure"
                                     "csvw:datatype" :double
                                     "csvw:titles" "measure"}]})
         ds1 (data.validation/as-dataset
-             ^{:type :datahost.types/seq-of-maps} [{"measure" 1.0 "attr1" "a"}
-                                                   {"measure" 1.0 "attr1" "b"}
-                                                   {"measure" 4.5 "attr1" "d"}] {})
+             ^{:type :datahost.types/seq-of-maps} [{"measure" 1.0 "dim1" "a"}
+                                                   {"measure" 1.0 "dim1" "b"}
+                                                   {"measure" 4.5 "dim1" "d"}] {})
         ds2 (data.validation/as-dataset
-             ^{:type :datahost.types/seq-of-maps} [{"measure" 1.2 "attr1" "a"}
-                                                   {"measure" 3.0 "attr1" "c"}
-                                                   {"measure" 4.5 "attr1" "d"}] {})
+             ^{:type :datahost.types/seq-of-maps} [{"measure" 1.2 "dim1" "a"}
+                                                   {"measure" 3.0 "dim1" "c"}
+                                                   {"measure" 4.5 "dim1" "d"}] {})
         result (data.delta/delta-dataset ds1 ds2 {:row-schema row-schema
                                                   :measure-column "measure"
-                                                  :coords-columns ["attr1"]})]
+                                                  :coords-columns ["dim1"]})]
     (is (= 4 (tc/row-count result)))
     (is (= 4 (count (set (tc/->array result "datahost.row/id")))))
     (is (= 1 (count (disj (set (tc/->array result "datahost.row.id/previous")) Long/MIN_VALUE))))))
