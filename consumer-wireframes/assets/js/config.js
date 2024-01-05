bd1b0615bad899757e59a63f62ae4f829d74d581
Tayyeb Rafique <tayyebrafique@Tayyebs-MBP.cust.communityfibre.co.uk>
2024-01-05 16:16:44 +0000

    Pushing the ui container to GCP thorugh the CI

--- consumer-wireframes/assets/js/config.js
+++ consumer-wireframes/assets/js/config.js
@@ -1,3 +1,7 @@
 // const domain = "http://localhost:3000"
 // const domain = "https://ldapi-prototype.gss-data.org.uk"
 const domain = "https://ldapi-dev.gss-data.org.uk"
+
+// const domain = process.env.LDAPI_BASE_URL
+
+// Not being used - env contains domain and is being passed through the template to clientside js
