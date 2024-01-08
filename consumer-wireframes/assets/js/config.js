d6ff75b2a6926621fcf15e6788eaa72cc1461f84
Tayyeb Rafique <tayyebrafique@Tayyebs-MBP.cust.communityfibre.co.uk>
2024-01-08 16:41:03 +0000

    changed config js to take env variable as backend url

--- consumer-wireframes/assets/js/config.js
+++ consumer-wireframes/assets/js/config.js
@@ -1,3 +1,4 @@
 // const domain = "http://localhost:3000"
 // const domain = "https://ldapi-prototype.gss-data.org.uk"
-const domain = "https://ldapi-dev.gss-data.org.uk"
+// const domain = "https://ldapi-dev.gss-data.org.uk"
+const domain = process.env.LDAPI_BASE_URL
